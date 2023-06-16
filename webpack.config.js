e55a6935f1e18a120c5f043588942a9918b864d1
anis <anis@syncc.app>
2023-06-16 10:53:54 +0100

    update webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    main: './src/main.js',
-    // Add more entry points for other JavaScript files
-  },
+  entry: './src/main.js',
   output: {
-    filename: 'sdk.js', // Set the output filename to 'sdk.js' for the non-minified version
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    library: 'SDK', // Export the bundled module as a global variable named 'SDK'
-    libraryTarget: 'umd', // Use Universal Module Definition (UMD) to support multiple environments
   },
   optimization: {
     minimize: true, // Enable minification
@@ -17,6 +12,16 @@ module.exports = {
   devtool: 'source-map', // Generate source map file
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
       {
         test: /\.css$/, // Match CSS files
         use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
